e7898413e3021ff57cf6121ac4c4bcd8f55b4128
egor <pichugin.egor1993@gmail.com>
2025-11-23 13:45:47 +0100

    created client that allows to create new submission forms with validation for fields

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <h1>Welcome to Nuxt!</h1>
+    <p>This is the index page (pages/index.vue).</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Page-level logic can go here.
+</script>
